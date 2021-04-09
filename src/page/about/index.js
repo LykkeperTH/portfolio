8cdf478f6e228cfc8570996f054de04e840182c9
LykkeperTH <72471862+LykkeperTH@users.noreply.github.com>
2021-04-09 21:15:23 +0700

    detail

--- src/page/about/index.js
+++ src/page/about/index.js
@@ -1,4 +1,12 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 const About = () => {
-  return <div>About Test </div>;
+  return (
+    <Container fluid="md">
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  );
 };
 export default About;
--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,4 +1,16 @@
+import { Container, Jumbotron, Row } from "react-bootstrap";
+
 const Home = () => {
-  return <div>Home Test </div>;
+  return (
+    <Container fluid="md">
+      <Jumbotron style={{ marginTop: "10px" }}>
+        <h1>Fluid jumbotron</h1>
+        <p>
+          This is a modified jumbotron that occupies the entire horizontal space
+          of its parent.
+        </p>
+      </Jumbotron>
+    </Container>
+  );
 };
 export default Home;
