036b642ba4d7f67d41ad18ec22c5af76aafd600a
Kittinan <rekise1@hotmail.com>
2021-04-15 13:20:52 +0700

    detail-JSON

--- src/page/about/index.js
+++ src/page/about/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Media, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 const About = () => {
   return (
